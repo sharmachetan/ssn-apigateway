50c95c6c7d834e20350fc5583d65089852d6c310
chetan <sharmachetan6@gmail.com>
2020-06-14 19:16:50 +0100

    added Swagger configuration file and made it running

--- src/main/java/com/srn/service/init/ServiceRegistryStarter.java
+++ src/main/java/com/srn/service/init/ServiceRegistryStarter.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableFeignClients
 @EnableEurekaServer
+@ComponentScan(basePackages ="com.srn" )
 public class ServiceRegistryStarter {
 
 
